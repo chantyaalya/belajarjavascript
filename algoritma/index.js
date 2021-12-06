08a4f0c48bcdf65c51f2a53cf1647162976f9139
chantyaalya <chantyaalya11@gmail.com>
2021-12-06 14:56:22 +0700

    luas permukaan balok

---
+++ algoritma/index.js
@@ -0,0 +1,84 @@
+/**
+ * STUDY CASE 1
+ * 
+ * Buatlah sebuah rumus segitiga
+ * 
+ * ALGORITMA
+ * 1. Cari definisi dari rumus luas segitiga = 1/2 * alas * tinggi
+ * 2. Buat variable alas
+ * 3. Buat variable tinggi
+ * 4. Buat variable luas
+ * 5. Masukkan rumus luas segitiga ke dalam variabel luas
+ * 6. Proses Luas
+ * 7. Cetak Luas
+ * 
+ * PSEUDOCODE
+ * STORE alas WITH ANY NUMBER
+ * STORE tinggi WITH ANY NUMBER
+ * 
+ * SET luas WITH 1/2 MULTIPLY alas MULTIPLY tinggi
+ * PRINT luas
+ * 
+ */
+
+// var alas = 10;
+// var tinggi = 5;
+
+// var luas = 1 / 2 * alas * tinggi;
+// console.log(luas);
+
+/** 
+ * STUDY CASE 2
+ * 
+ * Buatlah luas lingkaran
+ * 
+ * ALGORITMA
+ * 1. Cari definisi dari luas lingkaran - pi * r *r
+ * 2. Buatlah variable r
+ * 3. Buarlah variable pi isi dengan angka 3.14
+ * 4. Buat variable luas
+ * 5. Isi luas dengan rumus = pi * r * r
+ * 6. Proses luas
+ * 7. Cetak luas
+ * 
+ * PSEUDOCODE
+ * STORE pi WITH 3.14
+ * STORE r WITH ANY NUMBER
+ * 
+ * Set luas WITH pi MULTIPLY r MULTIPLY r
+ * PRINT luas
+ */
+
+// var pi = 3.14
+// var r = 10
+// var luas = pi * r * r
+// console.log(luas);
+
+/**
+ * STUDY CASE 3
+ * 
+ * Buatlah rumus luas permukaan balok
+ * 
+ * ALGORITMA
+ * 1. Cari definisi dari permukaan balok 2 x (p * l + p * t + l * t)
+ * 2. buatlah variable p
+ * 3. buatlah variable l
+ * 4. buatlah variable t
+ * 5. Isi luas dengan rumus = 2 * (p*l+p*t+l*t)
+ * 6. proses luas
+ * 7. cetak luas
+ * 
+ * PSEUDOCODE
+ * STORE p WITH ANY NUMBER
+ * STORE l WITH ANY NUMBER
+ * STORE t WITH ANY NUMBER
+ * 
+ * Set luas WITH 2 MULTYPLY p MULTYPLY l PLUS 2 MULTIPLY p MULTIPLY t PLUS 2 MULTIPLY l MULTIPLY t
+ * PRINT luas
+ */
+
+var p = 2
+var l = 4
+var t = 5
+var luas = 2 * p * l + 2 * l * t + 2 * t * p
+console.log(luas);
